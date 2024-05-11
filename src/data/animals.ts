dc05aac3d126ffd70402cbbb43a0b5bc49dca047
M Haidar Hanif <git@mhaidarhanif.com>
2024-05-11 16:59:17 +0700

    feat: get animal by id

---
+++ src/data/animals.ts
@@ -0,0 +1,29 @@
+type Animal = {
+  id: number;
+  name: string;
+  scientificName: string;
+  speed: number;
+  class?: string;
+  domain?: string;
+  family?: string;
+};
+
+export const dataAnimals: Animal[] = [
+  {
+    id: 1,
+    name: "Bear",
+    scientificName: "Ursidae",
+    class: "Mammalia",
+    domain: "Eukaryota",
+    speed: 48, // km/h
+  },
+  {
+    id: 2,
+    name: "Cat",
+    scientificName: "Felis catus",
+    class: "Mammalia",
+    domain: "Eukaryota",
+    family: "Felidae",
+    speed: 48, // km/h
+  },
+];
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,35 @@
 import { Hono } from "hono";
 
+import { dataAnimals } from "./data/animals";
+
 const app = new Hono();
 
+// `/animals`     | `GET`    | Get all animals     |
+// `/animals/:id` | `GET`    | Get animal by id    |
+// `/animals`     | `POST`   | Add new animal      |
+// `/animals`     | `DELETE` | Delete all animals  |
+// `/animals/:id` | `DELETE` | Delete animal by id |
+// `/animals/:id` | `PUT`    | Update animal by id |
+
 app.get("/", (c) => {
-  return c.json({
-    message: "Hello world",
-  });
+  return c.json({ message: "Hello world" });
+});
+
+app.get("/animals", (c) => {
+  return c.json(dataAnimals);
+});
+
+app.get("/animals/:id", (c) => {
+  const id = Number(c.req.param("id"));
+
+  const animal = dataAnimals.find((animal) => animal.id === id);
+
+  if (!animal) {
+    c.status(404);
+    return c.json({ message: "Animal not found" });
+  }
+
+  return c.json(animal);
 });
 
 export default app;
