cf8612cc216d6f73baba05d1fd6ab96963c43fbd
M Haidar Hanif <git@mhaidarhanif.com>
2024-10-10 08:14:15 +0700

    feat: setup logger

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
+import { logger } from "hono/logger";
 
 import { dataAnimals } from "./data/animals.ts";
 import { prisma } from "./lib/db.ts";
 
 const app = new Hono();
+
+app.use(logger());
 
 // `/animals`     | `GET`    | Get all animals     |
 // `/animals/:id` | `GET`    | Get animal by id    |
